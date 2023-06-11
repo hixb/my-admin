83ac8d5145d35c7d8d58abe8f9f71597c74ff677
hixb <Hello16369@163.com>
2023-06-11 21:14:06 +0800

    style: layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,31 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 import { RouterView } from 'vue-router'
+import { NLayout, NLayoutFooter, NLayoutHeader, NLayoutSider } from 'naive-ui'
 </script>
 
 <template>
-  <RouterView />
+  <NLayout style="height: 100vh">
+    <NLayoutHeader bordered style="height: 64px; padding: 24px">
+      颐和园路
+    </NLayoutHeader>
+    <NLayout has-sider position="absolute" style="top: 64px; bottom: 64px">
+      <NLayoutSider
+        :native-scrollbar="false"
+        bordered
+        content-style="padding: 24px;"
+      />
+      <NLayout :native-scrollbar="false" content-style="padding: 24px;">
+        <RouterView />
+      </NLayout>
+    </NLayout>
+    <NLayoutFooter
+      bordered
+      position="absolute"
+      style="height: 64px; padding: 24px"
+    >
+      城府路
+    </NLayoutFooter>
+  </NLayout>
 </template>
 
 <style scoped></style>
